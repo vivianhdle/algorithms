bd559341a301e51d1ec03e81d0996c5ef585a556
Vivian Le <vivianhdle@gmail.com>
2019-07-15 12:48:49 -0700

    added search function for linked list data structure

--- linkedlists.js
+++ linkedlists.js
@@ -1,25 +1,28 @@
 class LinkedList{
     constructor(){
-        head = null;
-        tail = null;
+        this.head = null;
+        this.tail = null;
     }
+    //if given a target, add after that target
+    //if target is null, add to the beginning
+    //if there is no target, add to the end
     insertNode(newValue, target){
         const newNode = {
             value:newValue,
             next:null
         }
-        if (target === undefined){
+        if (target === undefined){//add to the end of list
             if (!this.head){
                 this.head = newNode;
             } else {
                 this.tail.next = newNode;
             }
             this.tail = newNode;
-        } else if (target===null){
+        } else if (target===null){ //add to beginning
             newNode.next = this.head;
             this.head = newNode;
         } else {
-            if (target.next === null){
+            if (target.next === null){ //add behind another node
                 this.tail = newNode;
             } else {
                 newNode.next = target.next;
@@ -27,7 +30,24 @@ class LinkedList{
             target.next = newNode;
         }
     }
+    searchNode(targetValue){
+        let looking = true;
+        let checking = this.head;
+        while ( looking ) {
+            if (checking.value === targetValue){
+                looking = false;
+                return checking;
+            } else {
+                checking = checking.next;
+            }
+        }
+    }
 }
 
 
 const linkedList = new LinkedList()
+linkedList.insertNode(5);
+linkedList.insertNode(7);
+linkedList.insertNode(9);
+console.log(linkedList);
+console.log(linkedList.searchNode(9));
